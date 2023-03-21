324c241c0eb9c3b4672bfe749ffd1d295f98c002
MR.Flame <67482496+MR-Flame-exe@users.noreply.github.com>
2023-03-21 02:13:12 +0100

    Add files via upload

---
+++ util/config.js
@@ -0,0 +1,13 @@
+let config;
+
+try {
+    config = require("./config.json");
+} catch (error) {
+    config = null;
+}
+
+exports.token = config ? config.token : process.env.token;
+exports.twitch_token = config ? config.twitch_token : process.env.twitch_token;
+exports.twitch_id = config ? config.twitch_id : process.env.twitch_id;
+exports.twitch_secret = config ? config.twitch_secret : process.env.twitch_secret;
+exports.CLIENT_ID = config ? config.CLIENT_ID : process.env.CLIENT_ID;
